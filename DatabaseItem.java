744ffa794f26c9efccd3d2cccba0c23736c37116
mkharismaramadhan <47825379+mkharismaramadhan@users.noreply.github.com>
2019-02-21 17:59:04 +0700

    modul1

---
+++ DatabaseItem.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class DatabaseItem here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatabaseItem
+{
+    // instance variables - replace the example below with your own
+    private String[] listItem;
+
+    /**
+     * Constructor for objects of class DatabaseItem
+     */
+    public void additem()
+    {
+    }
+    
+    public void removeItem()
+    {
+    }
+    
+    public void getItemDatabase()
+    {
+    }
+    
+    
+        
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+}
---
+++ JStore.java
@@ -0,0 +1,19 @@
+
+public class JStore
+{
+    // instance variables - replace the example below with your own
+    public void main(String[] args)
+    {        
+    }
+    
+    public JStore()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+}
