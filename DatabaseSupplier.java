168fd50a96a64bae784b0d6e9c76915fc927a58d
mkharismaramadhan <47825379+mkharismaramadhan@users.noreply.github.com>
2019-02-28 17:42:08 +0700

    modul2
    modul2

--- DatabaseSupplier.java
+++ DatabaseSupplier.java
@@ -1,37 +1,59 @@
 
 /**
- * Write a description of class DatabaseSupplier here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Class DatabaseSupplier
+ * Berisi mengenai database Supplier
+ * @author M. Kharisma Ramadhan
+ * @version 1.0
+ * since 2019
  */
 public class DatabaseSupplier
 {
+    /**
+     * class dari DatabaseSupplier
+     */
     // instance variables - replace the example below with your own
     private String[] listSupplier;
     private Supplier supplier;
 
     /**
-     * An example of a method - replace this comment with your own
+     * Method untuk menambah supplier
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  supplier
+     * @return true
      */
     public boolean addSupplier(Supplier supplier)
     {
         return true;
     }
     
+    /**
+     * Method untuk menghapus supplier
+     *
+     * @param  supplier
+     * @return unknown
+     */
     public void removeSupplier(Supplier supplier)
     {
         return;
     }
     
+    /**
+     * Method untuk mengambil nilai dari var supplier
+     *
+     * @param  tidak ada
+     * @return supplier
+     */
     public Supplier getSupplier()
     {
         return supplier;
     }
     
+    /**
+     * Method untuk mengambil listSupplier
+     *
+     * @param  tidak ada
+     * @return listSupplier
+     */
     public String[] getListSupplier()
     {
         return listSupplier;
